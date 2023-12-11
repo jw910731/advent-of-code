627bd6ccdc14095b2eeb5eefe870835dfede20af
jw910731 <jw910731@gmail.com>
2023-12-12 00:17:29 +0800

    Complete day 4 challenge.

--- src/aoc/y2023/day4.rs
+++ src/aoc/y2023/day4.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 
 pub fn day4(input: String) -> String {
-    input
+    let pt: Vec<_> = input
         .lines()
         .map(|line| {
             let (winning_numbers, owned_numbers) = line
@@ -17,17 +17,28 @@ pub fn day4(input: String) -> String {
                 })
                 .collect_tuple()
                 .unwrap();
-            let winning_points = owned_numbers
+            owned_numbers
                 .iter()
                 .filter(|x| winning_numbers.contains(x))
                 .collect_vec()
-                .len() as i32;
-            if winning_points > 0 {
-                1 << winning_points - 1
-            } else {
-                0
-            }
+                .len() as i32
         })
+        .collect();
+    (0..pt.len())
+        .fold(Vec::default(), |acc, idx| {
+            let next_value = (0..idx)
+                .rev()
+                .zip(pt.iter())
+                .map(|(i, p)| (i as i32) < *p)
+                .map(|r| r as i32)
+                .zip(acc.iter())
+                .map(|(a, b)| a * (*b))
+                .sum::<i32>();
+            acc.into_iter()
+                .chain([1 + next_value].into_iter())
+                .collect()
+        })
+        .iter()
         .sum::<i32>()
         .to_string()
 }
